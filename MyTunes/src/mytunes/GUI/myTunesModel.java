257f7ab5cb0862aa31607335b5d63492ebeaac70
Sub-Z3r0 <andersen19@gmail.com>
2017-11-28 11:45:26 +0100

    model changes
    constructor, getters and setters stringproperty import etc

--- MyTunes/src/mytunes/GUI/myTunesModel.java
+++ MyTunes/src/mytunes/GUI/myTunesModel.java
@@ -5,11 +5,109 @@
  */
 package mytunes.GUI;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.media.Media;
+
 /**
  *
  * @author mr.Andersen
  */
 public class myTunesModel 
 {
+    private StringProperty fileName;
+    private StringProperty path;
+    private StringProperty album;
+    private StringProperty artist;
+    private StringProperty year;
+    private StringProperty title;
+    
+    private Media tune;
+    
+    public myTunesModel() 
+    {
+        this(null, null, null);
+    }
+
+    public myTunesModel(String fileName, String path, Media tune) 
+    {
+        this.fileName = new SimpleStringProperty(fileName);
+        this.path = new SimpleStringProperty(path);
+        this.tune = tune;
+    }
+
+    public StringProperty getFileName() 
+    {
+        return fileName;
+    }
+
+    public void setFileName(StringProperty fileName) 
+    {
+        this.fileName = fileName;
+    }
+
+    public StringProperty getPath() 
+    {
+        return path;
+    }
+
+    public void setPath(StringProperty path) 
+    {
+        this.path = path;
+    }
+
+    public StringProperty getAlbum() 
+    {
+        return album;
+    }
+
+    public void setAlbum(StringProperty album) 
+    {
+        this.album = album;
+    }
+
+    public StringProperty getArtist() 
+    {
+        return artist;
+    }
+
+    public void setArtist(StringProperty artist) 
+    {
+        this.artist = artist;
+    }
+
+    public StringProperty getYear() 
+    {
+        return year;
+    }
+
+    public void setYear(StringProperty year) 
+    {
+        this.year = year;
+    }
+
+    public StringProperty getTitle() 
+    {
+        return title;
+    }
+
+    public void setTitle(StringProperty title) 
+    {
+        this.title = title;
+    }
+
+    public Media getTune() 
+    {
+        return tune;
+    }
+
+    public void setTune(Media tune) 
+    {
+        this.tune = tune;
+    }
+    
+    
+    
+    
     
 }
